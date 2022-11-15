05b5dc907f7be687cc0fea56137fdd1943c450f7
Mostafa Mamun Emon <mostafaemon@Mostafas-MacBook-Pro.local>
2022-11-16 01:12:27 +0600

    category list complete mobile and pc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -157,122 +157,39 @@
                         <!--categori  -->
                         <div class="block-nav-categori">
                             <div class="block-title">
-                                <span>Categories</span>
+                                <span><i class="fa fa-th-large" style="font-size:px"></i> Categories</span>
                             </div>
                             <div class="block-content">
                                 <div class="clearfix"><span data-action="close-cat" class="close-cate"><span>Categories</span></span></div>
                                 <!-- mobile categories -->
                                 <ul class="ui-categori">
-                                    <li class="parent">
+                                    @php $sl = 0; @endphp
+                                    @foreach($categories as $category)
+                                    @php 
+                                        $sub_categories = get_sub_category($category->id); 
+                                        $sl = $sl + 1;
+                                    @endphp
+                                    <li class="@if(count($sub_categories) > 0) parent @endif @if($sl > 11) cat-link-orther @endif">
                                         <a href="">
                                             <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Electronics</span>
+                                            <span class="text">&nbsp;&nbsp;{{ $category->category_name }}</span>
                                         </a>
+                                        @if(count($sub_categories) > 1)
                                         <span class="toggle-submenu"></span>
                                         <div class="submenu">
                                             <ul class="categori-list clearfix">
                                                 <li class="col-sm-3">
-                                                    <strong class="title"><a href="">Smartphone</a></strong>
                                                     <ul>
-                                                        <li><a href="">Skirts    </a></li>
-                                                        <li><a href="">Jackets</a></li>
-                                                        <li><a href="">Jumpusuits</a></li>
-                                                        <li><a href="">Scarvest</a></li>
-                                                        <li><a href="">T-Shirts</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="col-sm-3">
-                                                    <strong class="title"><a href="">Camera</a></strong>
-                                                    <ul>
-                                                        <li><a href="">Skirts    </a></li>
-                                                        <li><a href="">Jackets</a></li>
-                                                        <li><a href="">Jumpusuits</a></li>
-                                                        <li><a href="">Scarvest</a></li>
-                                                        <li><a href="">T-Shirts</a></li>
+                                                    @foreach($sub_categories as $sub_category)
+                                                        <li style="padding-top:10px"><a href="">{{ $sub_category->sub_category_name }}</a></li>
+                                                    @endforeach
                                                     </ul>
                                                 </li>
                                             </ul>
                                         </div>
+                                        @endif
                                     </li>
-                                    <li class="parent">
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Sports & Outdoors</span>
-                                        </a>
-                                        <span class="toggle-submenu"></span>
-                                        <div class="submenu">
-                                            <ul class="categori-list">
-                                                <li class="col-sm-3">
-                                                    <strong class="title"><a href="">Smartphone</a></strong>
-                                                    <ul>
-                                                        <li><a href="">Skirts    </a></li>
-                                                        <li><a href="">Jackets</a></li>
-                                                        <li><a href="">Jumpusuits</a></li>
-                                                        <li><a href="">Scarvest</a></li>
-                                                        <li><a href="">T-Shirts</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="col-sm-3">
-                                                    <strong class="title"><a href="">washing machine</a></strong>
-                                                    <ul>
-                                                        <li><a href="">Skirts    </a></li>
-                                                        <li><a href="">Jackets</a></li>
-                                                        <li><a href="">Jumpusuits</a></li>
-                                                        <li><a href="">Scarvest</a></li>
-                                                        <li><a href="">T-Shirts</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Health & Beauty</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Bags, Shoes & Accessories</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Toys & Hobbies</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Computers & Networking</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Laptop & Accessories</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Jewellry & Watches</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Flashlights & Lamps</span>
-                                        </a>
-                                    </li>
-                                    <li class="cat-link-orther">
-                                        <a href="">
-                                            <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
-                                            <span class="text">&nbsp;&nbsp;Cameras & Photo</span>
-                                        </a>
-                                    </li>
+                                    @endforeach
                                 </ul>
                                 <div class="view-all-categori">
                                     <a  class="open-cate btn-view-all">All Categories</a>
@@ -397,9 +314,13 @@
                             </div>
                             <div class="block-content">
                                 <ul class="ui-categori">
+                                    @php $sl = 0; @endphp
                                     @foreach($categories as $category)
-                                    @php $sub_categories = get_sub_category($category->id); @endphp
-                                    <li @if(count($sub_categories) > 0) class="parent" @endif>
+                                    @php 
+                                        $sub_categories = get_sub_category($category->id); 
+                                        $sl = $sl + 1;
+                                    @endphp
+                                    <li class="@if(count($sub_categories) > 0) parent @endif @if($sl > 11) cat-link-orther @endif">
                                         <a href="">
                                             <span class="ctg-icon"><i class="fa fa-th-large"></i></span>
                                             <span class="text">{{ $category->category_name }}</span>
@@ -407,9 +328,9 @@
                                         <span class="toggle-submenu"></span>
                                         <div class="submenu">
                                             <ul class="categori-list clearfix">
-                                                <li class="col-sm-1">
+                                                <li class="col-sm-12">
                                                     @foreach($sub_categories as $sub_category)
-                                                    <strong class="title"><a href="">Smartphone</a></strong>
+                                                    <strong class="title"><a href="">{{ $sub_category->sub_category_name }}</a></strong>
                                                     @endforeach
                                                 </li>
                                             </ul>
@@ -1513,7 +1434,7 @@
             </div>
         </main><!-- end MAIN -->
         
-        <footer class="site-footer footer-opt-3" style="padding-top:20px">
+        <footer class="site-footer footer-opt-3" style="padding-top:20px;padding-bottom:100px">
             <div class="footer-content" style="background-image: url(images/media/bg-footer.jpeg);">
                 <div class="block-newletter">
                     <div class="container">
