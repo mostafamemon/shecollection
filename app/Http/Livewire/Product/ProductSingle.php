b58c3a7a1be4de314010617eab1c3efc0801b025
Mostafa Mamun Emon <mostafaemon@Mostafas-MacBook-Pro.local>
2022-11-17 23:24:33 +0600

    single product design start

---
+++ app/Http/Livewire/Product/ProductSingle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use Livewire\Component;
+
+class ProductSingle extends Component
+{
+    public function render()
+    {
+        return view('livewire.product.product-single');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Livewire\Home\Index;
 use App\Http\Livewire\Product\ProductList;
+use App\Http\Livewire\Product\ProductSingle;
 
 // Route::get('/',[HomeController::class, 'index']);
 Route::get('/',Index::class);
 Route::get('/product-list',ProductList::class);
+Route::get('/product',ProductSingle::class);
