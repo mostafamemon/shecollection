3fa0fdaf374542a7ac91c304cf91d1cb587f87e5
Mostafa Mamun Emon <mostafaemon@Mostafas-MacBook-Pro.local>
2022-11-19 02:13:34 +0600

    login complete

--- app/Http/Livewire/Auth/Login.php
+++ app/Http/Livewire/Auth/Login.php
@@ -2,10 +2,53 @@
 
 namespace App\Http\Livewire\Auth;
 
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use App\Models\User;
+use Hash;
+use Auth;
 
 class Login extends Component
 {
+    use LivewireAlert;
+
+    public $username    = "";
+    public $name        = "";
+    public $email       = "";
+    public $phone       = "";
+    public $password    = "";
+    public $address     = "";
+
+    public $login_username = "";
+    public $login_password = "";
+
+    public function register()
+    {
+        $user               = new User();
+        $user->username     = $this->username;
+        $user->name         = $this->name;
+        $user->email        = $this->email;
+        $user->phone        = $this->phone;
+        $user->password     = Hash::make($this->password);
+        $user->address      = $this->address;
+        $user->save();
+
+        if(Auth::attempt(['username' => $this->username, 'password' => $this->password])){
+            return redirect('/');
+        }
+    }
+
+    public function login()
+    {
+        if(Auth::attempt(['username' => $this->login_username, 'password' => $this->login_password])){
+            return redirect('/');
+        } else {
+            $this->confirm('Invalid username / password !', [
+                'icon' => 'warning'
+            ]);
+        }
+    }
+
     public function render()
     {
         return view('livewire.auth.login');
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -2,38 +2,42 @@
     <div class="page-content">
         <div class="row">
             <div class="col-sm-6">
+                <form wire:submit.prevent="login">
                 <div class="box-authentication">
                     <h3>Already registered?</h3>
-                    <label for="login_username">Username</label>
-                    <input type="text" class="form-control" id="login_username">
-                    <label for="login_pass">Password</label>
-                    <input type="password" class="form-control" id="login_pass">
+                    <label for="login_username">Username *</label>
+                    <input type="text" wire:model.lazy="login_username" class="form-control" id="login_username">
+                    <label for="login_pass">Password *</label>
+                    <input type="password" wire:model.lazy="login_password" class="form-control" id="login_pass">
                     <button class="button"><i class="fa fa-lock"></i> Sign in</button>
                 </div>
+                </form>
             </div>
             <div class="col-sm-6">
-                <div class="box-authentication">
-                    <h3>Create an account</h3>
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" id="username">
+                <form wire:submit.prevent="register">
+                    <div class="box-authentication">
+                        <h3>Create an account</h3>
+                        <label for="username">Username *</label>
+                        <input type="text" wire:model.lazy="username" required class="form-control" id="username">
 
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" id="password">
+                        <label for="password">Password *</label>
+                        <input type="password" wire:model.lazy="password" required class="form-control" id="password">
 
-                    <label for="name">Name</label>
-                    <input type="text" class="form-control" id="name">
+                        <label for="name">Name *</label>
+                        <input type="text" wire:model.lazy="name" required class="form-control" id="name">
 
-                    <label for="phone">Phone</label>
-                    <input type="phone" class="form-control" id="phone">
+                        <label for="phone">Phone *</label>
+                        <input type="phone" wire:model.lazy="phone" required class="form-control" id="phone">
 
-                    <label for="email">Password</label>
-                    <input type="text" class="form-control" id="email">
+                        <label for="email">Email</label>
+                        <input type="text" wire:model.lazy="email" class="form-control" id="email">
 
-                    <label for="address">Address</label>
-                    <input type="text" class="form-control" id="address">
+                        <label for="address">Address *</label>
+                        <input type="text" wire:model.lazy="address" required placeholder="delivery address" class="form-control" id="address">
 
-                    <button class="button"><i class="fa fa-user"></i> Create an account</button>
-                </div>
+                        <button class="button"><i class="fa fa-user"></i> Create an account</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
