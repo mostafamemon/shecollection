05c244085b63e9cbb53d576346fb03f15f15bea4
Mostafa Mamun Emon <mostafaemon@Mostafas-MacBook-Pro.local>
2022-11-18 21:18:52 +0600

    search bar category complete

--- resources/views/livewire/home/search-bar-category.blade.php
+++ resources/views/livewire/home/search-bar-category.blade.php
@@ -8,7 +8,7 @@
             @php $total_search_bar_category = count($search_bar_categories); $sl = 0; @endphp
             @foreach($search_bar_categories as $search_bar_category)
                 @php $sl = $sl + 1 @endphp
-                <div class="@if($sl == 1) search-bottom-text @else search-bottom-text-left @endif mobile-hide">{{ $search_bar_category->category_name }}</div>
+                <div class="@if($sl == 1) search-bottom-text @else search-bottom-text-left @endif mobile-hide"><a href="product-list?category={{ $search_bar_category->id }}" style="color:white">{{ $search_bar_category->category_name }}</a></div>
                 @if($sl < $total_search_bar_category)
                 <div class="search-bottom-text-border mobile-hide">&nbsp;</div>
                 @endif
